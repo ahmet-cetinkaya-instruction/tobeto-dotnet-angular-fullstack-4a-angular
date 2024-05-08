ad94936081fc1f3dad4cef8c0e5a9c2e3cac8469
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-08 15:15:14 +0300

    add component notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 // Component: Angular tarafında bir görünüm ve işlev için küçük parçalardır.
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root', // template içinde kullanılacak etiket adı
+
+  standalone: true, // Angular 17 sonrası için varsayılan hale geldi. Standalone componentler herhangi bir Module yapısına bağlı kalmadan var olabilirler.
+  imports: [NavbarComponent, FooterComponent], // Import edilecek modülleri ve angular yapılarını belirtir.
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'northwind';
+export class AppComponent { // Değişkenler ve fonksiyonlar burada tanımlanır.
+  title: string = 'Northwind Market'; // State: Angular state değerlerini tutar ve değer değişiklikleri canlı olarak takip eder.
+
+  cartCount: number = 0; // Sepetteki ürün sayısını tutar.
+
+  onAddProductToCart(): void { // Sepete ürün ekleme işlemi
+    this.cartCount += 1;
+
+    console.log('Ürün sepete eklendi.');
+  }
 }
+
+// Angular 17 öncesinde varsayılan olarak componentler Module yapısında tanımlanıyorlardı.
+// Angular Module Örneği
+// @NgModule({
+//   declarations: [AppComponent], // Component, Directive ve Pipe'ları tanımlar.
+//   imports: [], // Dışarıdan alınan modülleri ekler.
+//   providers: [], // Servisleri ekler.
+//   bootstrap: [AppComponent], // Uygulamanın başlangıç componentini belirler.
+// })
+// export class AppModule {}
