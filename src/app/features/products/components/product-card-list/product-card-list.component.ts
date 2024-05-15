586d9b56fd2ce3097f16c4277ed143cdb005c6d6
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-15 13:33:36 +0300

    optimize product card list component for better performance

--- src/app/features/products/components/product-card-list/product-card-list.component.ts
+++ src/app/features/products/components/product-card-list/product-card-list.component.ts
@@ -10,6 +10,7 @@ import {
 import { ProductListItem } from '../../models/product-list-item';
 import { CardComponent } from '../../../../shared/components/card/card.component';
 import { ProductsService } from '../../services/products.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-product-card-list',
@@ -32,10 +33,12 @@ export class ProductCardListComponent implements OnInit {
   }
 
   getProductList() {
-    const request = this.productsService.getList().subscribe((productList) => {
-      this.productList = productList;
-      request.unsubscribe();
-    });
+    this.productsService
+      .getList()
+      .pipe(take(1))
+      .subscribe((productList) => {
+        this.productList = productList;
+      });
   }
 
   onViewProduct(product: ProductListItem) {
