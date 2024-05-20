249405468233e17503376d61a6fd76c95591a691
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-20 16:17:32 +0300

    add lazy loading

--- src/app/features/products/components/product-card-list/product-card-list.component.ts
+++ src/app/features/products/components/product-card-list/product-card-list.component.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   EventEmitter,
+  Inject,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   Output,
   SimpleChanges,
@@ -26,20 +28,23 @@ import { PaginatedList } from '../../../../core/models/paginated-list';
   styleUrl: './product-card-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardListComponent implements OnInit, OnChanges {
+export class ProductCardListComponent implements OnInit, OnChanges, OnDestroy {
   @Input() filterByCategoryId: number | null = null;
   @Output() viewProduct = new EventEmitter<ProductListItem>();
 
-  productList!: PaginatedList<ProductListItem>;
+  productList?: PaginatedList<ProductListItem>;
   readonly pageSize: number = 12;
 
   constructor(
     private productsService: ProductsService,
-    private change: ChangeDetectorRef
+    private change: ChangeDetectorRef,
+    @Inject(DOCUMENT) private document: Document
   ) {}
 
   ngOnInit(): void {
     this.getProductList();
+
+    this.document.addEventListener('scroll', () => this.onScroll());
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -51,6 +56,10 @@ export class ProductCardListComponent implements OnInit, OnChanges {
       this.getProductList();
   }
 
+  ngOnDestroy(): void {
+    this.document.removeEventListener('scroll', () => {});
+  }
+
   getProductList(page: number = 1) {
     this.productsService
       .getList(page, this.pageSize, {
@@ -58,7 +67,12 @@ export class ProductCardListComponent implements OnInit, OnChanges {
       })
       .pipe(take(1))
       .subscribe((productList) => {
-        this.productList = productList;
+        if (!this.productList) this.productList = productList;
+        else {
+          this.productList!.items.push(...productList.items);
+          this.productList!.pageIndex = productList.pageIndex;
+          this.productList!.totalItems = productList.totalItems;
+        }
         this.change.markForCheck();
       });
   }
@@ -69,6 +83,15 @@ export class ProductCardListComponent implements OnInit, OnChanges {
 
   onPageChange(newPage: number) {
     this.getProductList(newPage);
+  }
+
+  private onScroll() {
+    if (
+      this.productList &&
+      window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+      this.productList.pageIndex * this.pageSize < this.productList.totalItems
+    )
+      this.getProductList(this.productList.pageIndex + 1);
   }
 
   // get filteredProductList(): ProductListItem[] {
